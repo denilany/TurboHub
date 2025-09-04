a2af0c84d7d02522fd0ff050e3f22be939e40fa5
get <denilany1@gmail.com>
2025-09-04 14:14:19 +0300

    feat: enhance Button component with theme support and improved styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TouchableOpacity, Text, TouchableOpacityProps } from 'react-native';
 import clsx from 'clsx';
+import { useThemeStyles } from '../hooks/useThemeStyles';
 
 interface ButtonProps extends TouchableOpacityProps {
   title: string;
@@ -9,17 +10,21 @@ interface ButtonProps extends TouchableOpacityProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ title, className, textClassName, ...props }) => {
+  const { classes } = useThemeStyles();
+  
   return (
     <TouchableOpacity
       className={clsx(
         "w-full py-4 rounded-xl",
+        classes.button,
         className
       )}
       {...props}
     >
       <Text
         className={clsx(
-          "text-white text-center text-lg font-semibold",
+          "text-center text-lg font-semibold",
+          classes.buttonText,
           textClassName
         )}
       >
